b499259811669b6565efa98d9b98726611b7eddc
Fullmetal235 <jjbillupsbrown@gmail.com>
2020-06-30 14:54:59 -0500

    initial push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-6">column 1</div>
+        <div className="col-6">column 1</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
