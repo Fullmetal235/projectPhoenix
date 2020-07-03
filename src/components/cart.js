740355665122906bdecca74fc9aeae8c6e5d0d07
Fullmetal235 <jjbillupsbrown@gmail.com>
2020-07-02 19:14:10 -0500

    push test

--- src/components/cart.js
+++ src/components/cart.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Cart() {
     return (
-        <h3>Hello from cart</h3>
+        <h3>Hello from cart 2</h3>
     )
 }
-export default Cart
+export default Cart 
